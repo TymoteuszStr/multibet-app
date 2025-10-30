8d02d0598ea0ff9f1485b83d6026e7f633e12a27
Tymoteusz Strozniak <tymoteusz.strozniak@gmail.com>
2025-10-30 12:52:09 +0100

    add responsive desing

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -22,11 +22,48 @@ import MainDashboard from "@/components/MainDashboard.vue";
 .home-wrapper {
   display: grid;
   grid-template-columns: 400px auto 400px;
+  grid-template-areas: "left center right";
   align-items: start;
   gap: 0;
-
+  max-width: 2000px;
+  margin: 0 auto;
   > * {
     min-height: 200px;
+  }
+
+  > :nth-child(1) {
+    grid-area: left;
+  }
+  > :nth-child(2) {
+    grid-area: center;
+  }
+  > :nth-child(3) {
+    grid-area: right;
+  }
+}
+
+@media (max-width: 1024px) {
+  .home-wrapper {
+    grid-template-columns: 320px auto;
+    grid-template-areas:
+      "left center"
+      "right right";
+    gap: 16px;
+  }
+}
+
+@media (max-width: 600px) {
+  .home-wrapper {
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      "right"
+      "left"
+      "center";
+    gap: 12px;
+
+    > * {
+      min-height: unset;
+    }
   }
 }
 </style>
